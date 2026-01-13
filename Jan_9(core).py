bbbd7c1561e700a78f4f2d35ac9711c361962efd
farhankhanproff-gif <farhankhanproff@gmail.com>
2026-01-13 13:03:43 +0530

    Add files via upload
    -- created a RPSGame class
    -- Lets user play rock/paper/scissors against computer
    -- Uses random module

---
+++ Jan_9(core).py
@@ -0,0 +1,69 @@
+import random
+
+class RPSGame:
+    def __init__(self,  player_score=0, computer_score=0):
+        
+        self.player_score = player_score
+        self.computer_score = computer_score
+
+    def round(self):
+            player_name = input("Enter player name: ")
+            num_rounds = int(input("Enter number of rounds you want to play: "))
+            for num in range(num_rounds):
+
+             player_choice = input("Enter your move (rock/paper/scissors): ").lower().strip()
+             choices = ['rock', 'paper', 'scissors']
+             computer_choice = random.choice(choices)
+             print(f"Computer chose: {computer_choice}")
+
+
+
+
+             if player_choice == 'rock' and computer_choice == 'scissors':
+                self.player_score+=1
+                result = "You win"
+             elif player_choice =='rock' and computer_choice == 'paper':
+                self.computer_score+=1
+                result = "You lose"
+             elif player_choice =='scissors' and computer_choice == 'paper':
+                self.player_score+=1
+                result = "You win"
+             elif player_choice =='scissors' and computer_choice == 'rock':
+                self.computer_score+=1
+                result = "You lose"
+             elif player_choice =='paper' and computer_choice == 'rock':
+                self.player_score+=1
+                result = "You win"
+             elif player_choice =='paper' and computer_choice == 'scissors':
+                self.computer_score+=1
+                result = "You lose"
+             else:
+                result = "Draw"
+                pass
+             
+             
+             print(f"Result: {result}")
+            
+             def scoreboard():
+               print("-" * 30 + "SCOREBOARD" + "-" * 30)
+               print(f"{player_name} : {self.player_score}")
+               print(f"Computer : {self.computer_score}")
+            
+            scoreboard()
+             
+
+
+   
+
+Game = RPSGame()
+Game.round()
+      
+
+
+
+
+
+
+
+            
+        
